32f73f167ab8aed40d307bdcb5ba62d09a6a1f5a
lidongbo <lidongbo@caijingquan.com>
2017-12-20 16:48:25 +0800

    agent

--- src/main/java/cn/tellwhy/report/agent/AgentTest.java
+++ src/main/java/cn/tellwhy/report/agent/AgentTest.java
@@ -1,6 +1,7 @@
 package cn.tellwhy.report.agent;
 
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
 
 
 /*
@@ -15,15 +16,29 @@ Exception in thread "Attach Listener" java.lang.NoSuchMethodException: cn.tellwh
  * Created by aaa on 17-12-19.
  */
 public class AgentTest {
-    public static void agentmain(String args, Instrumentation inst)
-    {
+    public static void premain(String agentArgs, Instrumentation inst){
+        System.out.println(agentArgs);
+    }
+    
+    public static void agentmain(String args, Instrumentation inst) throws NoSuchFieldException, IllegalAccessException {
         System.out.println("start agent args=" + args);
-//        Class<?>[] classes = inst.getAllLoadedClasses();
-        Class<?>[] classes = inst.getInitiatedClasses(ClassLoader.getSystemClassLoader());
-        for (Class<?> cls : classes)
-        {
-            System.out.println(cls.getName());
+        Class<?>[] classes = inst.getAllLoadedClasses();
+//        Class<?>[] classes = inst.getInitiatedClasses(ClassLoader.getSystemClassLoader());
+//        inst.addTransformer();
+        for (Class<?> one : classes) {
+            if (one.getSimpleName().equals("RedisCacheProperties")) {
+                System.out.println(one.getName());
+//                System.out.println(one.toString());
+//                System.out.println(one.getDeclaredField("cache"));
+                Field attr = one.getDeclaredField("cache");
+                System.out.println(attr.toString());
+                System.out.println(attr.getGenericType());
+                System.out.println(attr.toGenericString());
+                System.out.println(attr.getType());
+            }
         }
+        
+//        inst.addTransformer(new TestClassFileTransformer());
         
         System.out.println("agent attached.");
     }
